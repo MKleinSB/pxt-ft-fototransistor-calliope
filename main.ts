1393e1afde314b909c1309795a275af1861159f4
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2019-06-05 17:21:32 +0200

    first release

--- main.ts
+++ main.ts
@@ -1,7 +1,76 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+const enum zustand {
+    //% block="unterbrochen"
+    unterbrochen = 20,
+    //% block="nicht unterbrochen"
+    nicht_unterbrochen = 40
+}
+
+//% weight=100 color=#0641f9  icon="\uf085"
+namespace fischertechnik {
+
+    const LightEventID = 3100;
+    let lastLightLevel = 0;
+    let Empfindlichkeit = 20;
+
+    /**
+     * Do something when the phototransistor at the specified pin is interrupted or not.
+     * @param pin with phototransistor connected
+     * @param LSzustand interrupted or not
+     */
+    //% subcategory="Phototransistor"
+    //% pin.fieldEditor="gridpicker" 
+    //% pin.fieldOptions.columns=4
+    //% blockId=LightLevel_create_event block="wenn Fototransistor an Pin %pin |  | %LSzustand"
+    export function onLightLevel(pin: AnalogPin, LSzustand: zustand, handler: () => void) {
+        control.onEvent(LightEventID + pin + LSzustand, EventBusValue.MICROBIT_EVT_ANY, handler);
+        control.inBackground(() => {
+            while (true) {
+                const LLevel = pins.analogReadPin(pin);
+                if ((LLevel > Empfindlichkeit) && (lastLightLevel <= Empfindlichkeit) && (LSzustand == zustand.unterbrochen)) {
+                    control.raiseEvent(LightEventID + pin + LSzustand, pin);
+                } else if ((LLevel <= Empfindlichkeit) && (lastLightLevel > Empfindlichkeit) && (LSzustand == zustand.nicht_unterbrochen)) {
+                    control.raiseEvent(LightEventID + pin + LSzustand, pin);
+                }
+                basic.pause(200);
+                lastLightLevel = LLevel
+            }
+        })
+    }
+
+    /**
+    * The read phototransistor block reads the pin that a Phototransistor is 
+    * connected to and returns true or false when the lightlevel ist lower or 
+    * higher than the lightsensitivity
+    * @param pin - is the pin which a phototransistor is connected to
+    * @param LSzustand - if interrupted or not
+    */
+    //% subcategory="Phototransistor"
+    //% blockId=readPhototransistor
+    //% block="Fototransistor an %pin| ist | %LSzustand"
+
+    export function readPhototransistor(pin: AnalogPin, LSzustand: zustand) {
+        const LiLevel = pins.analogReadPin(pin);
+        let Ergebnis = false;
+        if ((LiLevel > Empfindlichkeit) && (LSzustand == zustand.unterbrochen)) {
+            Ergebnis = true;
+        } else if ((LiLevel <= Empfindlichkeit) && (LSzustand == zustand.nicht_unterbrochen)) {
+            Ergebnis = true;
+        }
+        return Ergebnis
+    }
+
+    /**
+     * Set the sensitivity (analogvalue) of the photocell when 
+     * it´s interrupted. Normaly not necessary with original 
+     * fischertechnik parts. The predefined value is 20
+     * @param value - (analogvalue)
+     */
+    //% subcategory="Phototransistor"
+    //% value.defl=20
+    //% value.min=5 value.max=1023
+    //% blockId="SetLightSensitivity" block="setze Lichtempfindlichkeit auf %value"
+    export function SetLightSensitivity(value: number): void {
+        Empfindlichkeit = value;
+    }
+
+}
